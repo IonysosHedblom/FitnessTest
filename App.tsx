1702cedb8146c99efd10a1f41c80f348c0fb057f
Ionhedblom <ionysoshedblom@gmail.com>
2020-04-19 12:45:14 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Fitness Test app</Text>
+      <Ionicons name='ios-pizza' size={100} />
     </View>
   );
 }
